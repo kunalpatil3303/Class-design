73e9c6c386e967e1defcbf25f5b63d2e21cb1203
kunalpatil3303 <89775018+kunalpatil3303@users.noreply.github.com>
2021-11-16 19:39:50 +0530

    Add files via upload
    done

---
+++ refactoring_guru/Singleton.java
@@ -0,0 +1,54 @@
+package refactoring_guru;
+
+public final class Singleton {
+
+    private static volatile Singleton instance;
+
+    public String value;
+
+    private Singleton(String value) {
+        this.value = value;
+    }
+
+    public static Singleton getInstance(String value) {
+
+        Singleton result = instance;
+        if (result != null) {
+            return result;
+        }
+        synchronized(Singleton.class) {
+            if (instance == null) {
+                instance = new Singleton(value);
+            }
+            return instance;
+        }
+    }
+}
+
+class Singleton1 {
+    public static void main(String[] args) {
+        System.out.println("If you see the same value, then singleton was reused (yay!)" + "\n" +
+                "If you see different values, then 2 singletons were created (booo!!)" + "\n\n" +
+                "RESULT:" + "\n");
+        Thread threadFoo = new Thread(new ThreadFoo());
+        Thread threadBar = new Thread(new ThreadBar());
+        threadFoo.start();
+        threadBar.start();
+    }
+
+    static class ThreadFoo implements Runnable {
+        @Override
+        public void run() {
+            Singleton singleton = Singleton.getInstance("FOO");
+            System.out.println(singleton.value);
+        }
+    }
+
+    static class ThreadBar implements Runnable {
+        @Override
+        public void run() {
+            Singleton singleton = Singleton.getInstance("BAR");
+            System.out.println(singleton.value);
+        }
+    }
+}
